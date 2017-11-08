17caae3609923b2a53d7596592b35e3171ac8636
Vanelli <vincentanelliprog@gmail.com>
2017-11-08 15:11:45 +0100

    avancee structure et css

--- structurePage/nav.php
+++ structurePage/nav.php
@@ -6,6 +6,7 @@
 		<label class="<?php if($nom_module=='circuit' || $nom_module=='pageCircuit') echo 'active'?>"><a class="menu" href="index.php?module=circuit">Circuit</a></label>
 		<label class="<?php if($nom_module=='croisiere' || $nom_module=='pageCroisiere') echo 'active'?>"><a class="menu" href="index.php?module=croisiere">Croisière</a></label>
 		<label class="<?php if($nom_module=='promo' || $nom_module=='pagePromo') echo 'active'?>"><a class="menu" href="index.php?module=promo">Promo</a></label>
+		<label class="<?php if($nom_module=='connexion' || $nom_module=='pageConnexion') echo 'active'?>"><a class="menuBoutonCo" href="index.php?module=connexion">Connexion</a></label>
 
 	</p>
 </nav>
