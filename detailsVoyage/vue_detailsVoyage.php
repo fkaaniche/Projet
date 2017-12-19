2faaabb34c7933bc23cda8800dcace6905b63bc0
ebenazouz <erwan.benazouz@gmail.com>
2017-12-19 10:36:54 +0100

    module detailVoyage terminé et liens mis sur l'accueil

--- detailsVoyage/vue_detailsVoyage.php
+++ detailsVoyage/vue_detailsVoyage.php
@@ -4,25 +4,37 @@
 
 	class VueDetailsVoyage extends VueGenerique{
 		public function affiche($details){
-			$this->contenu="";
-			//TODO faire fonctionner ce truc puis mettre un script
-			$this->contenu="<h1>".$details['villeArriveeSejour']."</h1>
+			//TODO faire fonctionner le script
+			$this->contenu='<h1>'.$details["villeArriveeSejour"].'</h1>
 				
-				<!--<form>
-					<button id=\"btnDesc\" >Description</button>
-					<button id=\"btnActi\" >Activités</button>
-					<button id=\"btnForma\">Formalités</button>
-					<button id=\"btnTrans\">Transport</button>
-				</form> -->
-				<img src=\"".$details['illustrationSejour']."\" alt=\"Image Séjour\"/>
-				<p id=\"para\">".$details['descriptionDetail']."</p>
-				<p id=\"para\">".$details['descriptionActivite']."</p>
-				<p id=\"para\">".$details['descriptionFormalite']."</p>
-				<p id=\"para\">".$details['descriptionTransport']."</p>
+
+					<form>
+					<input type="button" value="Description" id="btnDesc" onclick="afficheDesc()"></input>
+					<input type="button" value="Activités" id="btnActi" onclick="afficheActi()"></input>
+					<input type="button" value="Formalités"id="btnForma" onclick="afficheForma()"></input>
+					<input type="button" value="Transport" id="btnTrans" onclick="afficheTrans()"></input>
+					</form>
+				<img src="'.$details["illustrationSejour"].'" alt="Image Séjour"/>
+				<p id="para">'.$details["descriptionDetail"].'</p>
 				
-				
-				
-			";
+				<script type="text/javascript">
+			
+					var p = document.getElementById("para");
+					function afficheDesc(){
+						p.textContent = "'.$details["descriptionDetail"].'"
+					}
+					function afficheActi(){
+						p.textContent = "'.$details['descriptionActivite'].'"
+					}
+					function afficheForma(){
+						p.textContent = "'.$details['descriptionFormalite'].'"
+					}
+					function afficheTrans(){
+						p.textContent = "'.$details['descriptionTransport'].'"
+					}
+				</script>
+
+			';
 			
 
 
