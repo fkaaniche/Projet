09a8d34f3411857df4733bc71c21bc7bc8a222d0
ebenazouz <32437220+ebenazouz@users.noreply.github.com>
2017-12-19 07:07:47 +0100

    Script retiré mais ça marche toujours pas

--- detailsVoyage/vue_detailsVoyage.php
+++ detailsVoyage/vue_detailsVoyage.php
@@ -5,37 +5,22 @@
 	class VueDetailsVoyage extends VueGenerique{
 		public function affiche($details){
 			$this->contenu="";
-			//TODO afficher le reste
-			//TODO ça devrait etre quasi fini
+			//TODO faire fonctionner ce truc puis mettre un script
 			$this->contenu="<h1>".$details['villeArriveeSejour']."</h1>
-				<script>
-					var p = document.getElementById('para');
-
-					function afficheDesc(){
-						p.textContent = '".$details['descriptionDetail']."'
-					}
-
-					function afficheActi(){
-						p.textContent = '".$details['descriptionActivite']."'
-					}
-
-					function afficheForma(){
-						p.textContent = '".$details['descriptionFormalite']."'
-					}
-
-					function afficheTrans(){
-						p.textContent = '".$details['descriptionTransport']."'
-					}
-
-				</script>
+				
 				<form>
-					<button id=\"btnDesc\" onclick=\"afficheDesc()\">Description</button>
-					<button id=\"btnActi\" onclick=\"afficheActi()\">Activités</button>
-					<button id=\"btnForma\" onclick=\"afficheForma()\">Formalités</button>
-					<button id=\"btnTrans\" onclick=\"afficheTrans()\">Transport</button>
+					<button id=\"btnDesc\" >Description</button>
+					<button id=\"btnActi\" >Activités</button>
+					<button id=\"btnForma\">Formalités</button>
+					<button id=\"btnTrans\">Transport</button>
 				</form>
 				<img src=\"".$details['illustrationSejour']."\" alt=\"Image Séjour\"/>
 				<p id=\"para\">".$details['descritptionDetail']."</p>
+				<p id=\"para\">".$details['descritptionActivite']."</p>
+				<p id=\"para\">".$details['descritptionFormalite']."</p>
+				<p id=\"para\">".$details['descritptionTransport']."</p>
+				
+				
 				
 			";
 
