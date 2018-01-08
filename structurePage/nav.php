d8cdd4e8e7dc4aa0ba8bef08cc08be71ed671134
Vanelli <vincentanelliprog@gmail.com>
2018-01-08 10:15:41 +0100

    fusion

--- structurePage/nav.php
+++ structurePage/nav.php
@@ -1,11 +1,7 @@
 <nav class="nav navbar-list">
-
-		<label class="<?php if($nom_module=='accueil') echo 'active'?>"><a class="menu" href="index.php?module=accueil">A propos</a></label>
-<!-- <label class="<?php if($nom_module=='sejour' || $nom_module=='pageSejour') echo 'active'?>"><a class="menu" href="index.php?module=sejour">Séjour</a></label>
- --> 		<!-- <label class="<?php if($nom_module=='circuit' || $nom_module=='pageCircuit') echo 'active'?>"><a class="menu" href="index.php?module=circuit">Circuit</a></label>
-		<label class="<?php if($nom_module=='croisiere' || $nom_module=='pageCroisiere') echo 'active'?>"><a class="menu" href="index.php?module=croisiere">Croisière</a></label>  -->
-<!-- 		<label class="<?php if($nom_module=='promo' || $nom_module=='pagePromo') echo 'active'?>"><a class="menu" href="index.php?module=promo">Promo</a></label>
- -->		<div class="boutonCo">
+	 	<a href="index.php" ><h1 id="titreSite2">Voyages</h1></a>	
+<div class="boutonCo">
+ 
 
 <?php
 
